8609af73a66d08b7d5cb1dc8d4fecd1137816b2b
Josiah Arkson <arksonjosiah@gmail.com>
2024-11-30 16:38:12 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import path from 'path'; // Import path for alias configuration
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path"; // Import path for alias configuration
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,11 +15,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Define alias for src folder
+      "@": path.resolve(__dirname, "src"), // Define alias for src folder
     },
   },
   server: {
-    host:'0.0.0.0',
-    port: 2000
-  }
+    host: "0.0.0.0",
+    port: 2000,
+  },
 });
