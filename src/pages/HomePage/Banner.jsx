60b2ec3341e00806364437d1dc1e6acfddaca829
Jephthah Yusuf <jephby@users.noreply.github.com>
2024-12-05 10:47:42 +0100

    Update Banner.jsx

--- src/pages/HomePage/Banner.jsx
+++ src/pages/HomePage/Banner.jsx
@@ -1,57 +1,42 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logo1 from "../../assets/itflogo.png";
+import logo2 from "../../assets/coat_of_arms.svg";
+import logo3 from "../../assets/logo.png";
+import logo4 from "../HomePage/image/img10.jpeg";
+import logo5 from "../HomePage/image/img1.jpeg";
+import logo6 from "../HomePage/image/img2.jpeg";
 import logo7 from "../HomePage/image/bimg.png";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from 'react-router-dom';
 import { BlurFade } from "../../components/ui/blur-fade";
+import HeaderThreeImages from "../../components/HeaderThreeImages/HeaderThreeImages";
 import HeaderTitle from "../../components/HeaderTitle/HeaderTitle";
+
+
 export default function Banner() {
+
+
   return (
-    <div className="bg-[whitesmoke] pb-[10px] min-h-[100vh] flex">
-      <div className="flex w-full max-w-[1300px] mx-auto pt-[100px] items-center">
-        <div className="basis-[65%]   min-h-[50px] px-[20px]">
-          <div className="relative rounded-[6px] px-3 text-sm/6 xl:text-xl text-black ring-1 ring-gray-200/10 hover:underline bg-transparent backdrop-blur-lg">
-            Announcing our next Phase of SUPA.
-            <Link
-              to="#"
-              className="ml-[5px] font-bold text-emerald-900 hover:text-yellow-400">
-              Read more...
-            </Link>
-          </div>
+    <div className="relative h-screen w-full overflow-hidden bg-slate-900">
+      {/* <img
+        alt=""
+        src={logo4}
+        className="absolute inset-0 w-full h-full  object-cover blur-xl"
+      /> */}
 
-          <HeaderTitle
-            withSplitType
-            title_font_size="45px"
-            title_top="Skill-Up Artisans (SUPA)"
-            description="Empowering artisans with tech-driven training, official licensing, access to essential toolkits, and promoting industry standards to elevate skills, enhance craftsmanship, and drive professional excellence across sectors. "
-            title_color="black"
-            description_color="#000"
-            description_max_width="900px"
-            centerText
-          />
-
-          <div className="flex gap-[20px] w-full justify-center items-center">
-            <BlurFade delay={0.25} inView>
-              <Link to="/register">
-                <button className="h-[48px] px-[40px] text-[14px] rounded-[40px] bg-emerald-900 hover:bg-emerald-900/80 text-[#fff]">
-                  Register
-                </button>
-              </Link>
-            </BlurFade>
-            <BlurFade delay={0.25} inView>
-              <button className="h-[48px] px-[40px] text-[14px] rounded-[40px] bg-yellow border-[1px] text-yellow-600 border-yellow-600">
-                View Programmes
-              </button>
-            </BlurFade>
-          </div>
-        </div>
-        <div className="basis-[35%] w-full  ] flex justify-center items-center">
-          <img
-            src={logo7}
-            alt="Artisan"
-            className="w-[90%] h-full object-cover hover:resize"
+      <div className="relative h-full w-full flex flex-col justify-between">
+        <div className="absolute inset-x-0 -top-20 -z-60 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-15rem)] aspect-video w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#fb6969] to-[#47d342] opacity-100 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
 
-        <div className="relative flex justify-center gap-2 items-center mx-auto max-w-7xl xl:px-0 px-4  pb-30 md:py-0 lg:py-10 sm:py-0 xl:py-20 -mt-1 bg-transparent">
+        <div className="relative flex justify-center gap-2 items-center mx-auto max-w-7xl xl:px-0 px-4 pt-10 pb-30 md:py-10 lg:py-20 sm:py-10 xl:py-40 -mt-1 bg-transparent">
           <div className="w-2/3 px-8 justify-center">
             <div className="w-full text-center mb-10">
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
@@ -104,6 +89,8 @@ export default function Banner() {
         </div>
 
       </div>
+
+      
     </div>
   );
 }
