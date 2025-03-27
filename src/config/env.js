c3572fe9e566d50792e2378fa58e599426fb4372
Jephthah Yusuf <jephby@users.noreply.github.com>
2025-03-27 09:36:23 +0100

    update signup

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = "https://supa-api.onrender.com/api";
-// export const API_BASE_URL = "http://localhost:3000/api" || "http://192.168.0.187:2000/api" || "http://192.168.137.1:2000/api";
+export const API_BASE_URL = "http://localhost:3000/api";
 // export const API_BASE_URL = "http://192.168.137.1:3000/api";
- export const API_BASE_URL = "http://192.168.0.187:3000/api";
+// export const API_BASE_URL = "http://192.168.0.187:3000/api";
 //export const API_BASE_URL = "https://supa-backend-api-freepoj.online/api";
